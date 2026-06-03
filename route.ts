82a73af7b16bd3ce3940c3cf98542a9cd4fc71dd
aurora200300 <raghurajdeep2003@gmail.com>
2026-06-03 22:32:51 +0545

    delete

--- route.ts
+++
@@ -1,3 +0,0 @@
-import Shell from "@/components/Shell";
-import { Dashboard, Practice, Lessons, WordPractice, SentencePractice, NewsPractice, SpeedTest, Analytics, Leaderboard, Settings } from "@/components/Sections";
-export default function Home(){return <Shell><div className="space-y-10"><Dashboard/><Practice/><Lessons/><WordPractice/><SentencePractice/><NewsPractice/><SpeedTest/><Analytics/><Leaderboard/><Settings/></div></Shell>}
