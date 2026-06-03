bf30929e60ecf2a07c3e2a12b465a3dde8539254
aurora200300 <raghurajdeep2003@gmail.com>
2026-06-03 21:45:15 +0545

    Add files via upload

---
+++ KeyboardHelper.tsx
@@ -0,0 +1,3 @@
+import Shell from "@/components/Shell";
+import { Dashboard, Practice, Lessons, WordPractice, SentencePractice, NewsPractice, SpeedTest, Analytics, Leaderboard, Settings } from "@/components/Sections";
+export default function Home(){return <Shell><div className="space-y-10"><Dashboard/><Practice/><Lessons/><WordPractice/><SentencePractice/><NewsPractice/><SpeedTest/><Analytics/><Leaderboard/><Settings/></div></Shell>}
